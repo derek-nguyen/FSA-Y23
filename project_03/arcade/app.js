b12958352431d9f697030f640913a1104fcf6ef4
Derek Nguyen <derek.nguyen02@gmail.com>
2023-03-10 23:38:48 -0800

    added snake head and body js

--- project_03/arcade/app.js
+++ project_03/arcade/app.js
@@ -1,6 +1,6 @@
 const snake = {
-    body: [[10, 5], [10, 6], [10, 7], [10, 8]],
-    nextDirection: [1, 0],
+    body: [[10, 5], [10, 6], [10, 7]],
+    head: [10, 8]
 }
 
 const gameState = {
@@ -8,13 +8,12 @@ const gameState = {
         defaultRow: 20,
         defaultColumn: 20,
     },
-    apple: [11, 8],
     snake: snake,
 }
 
 // game state
 const gameContainer = document.querySelector('body')
-let board  
+let board
 const rows = gameState.boardSize.defaultRow
 const cols = gameState.boardSize.defaultColumn
 const cells = []
@@ -29,7 +28,7 @@ document.getElementById('startGame').addEventListener('click', () => {
 
 function initGame() {
     board = gameContainer.appendChild(document.createElement('div'))
-    board.setAttribute('id','gameBoard')
+    board.setAttribute('id', 'gameBoard')
     for (let i = 0; i < rows; i++) {
         const row = []
         for (let j = 0; j < cols; j++) {
@@ -41,24 +40,71 @@ function initGame() {
         }
         cells.push(row)
     }
-}
 
+    renderSnake();
+}
 // REMOVE: at the end of testing
 initGame()
 
-function renderSnakeAndApple() {
-    
+function renderSnake() {
+    // render snake on board based on snake object
+    // add class snakeShape to the body
+    for (let i = 0; i < snake.body.length; i++) {
+        const [row, col] = snake.body[i]; //using array destructuring 
+        const cell = cells[row][col]
+        cell.classList.add('snakeShape')
+    }
+
+    // render snake head
+    const [snakeHeadRow, snakeHeadCol] = snake.head
+    cells[snakeHeadRow][snakeHeadCol].classList.add('snakeShape')
 }
+
+
+function removeSnake() {
+    for (let i = 0; i < snake.body.length; i++) {
+        const [row, col] = snake.body[i]; //using array destructuring 
+        const cell = cells[row][col]
+        cell.classList.remove('snakeShape')
+    }
+}
+
+
+document.addEventListener('keydown', (event) => {
+    removeSnake()
+    if (event.key === 'ArrowLeft') {
+        //need to change existing positions of snake.body and add [x,y-1]
+        snake.body = snake.body.map(([row, col]) => [row, col - 1])
+    }
+    else if (event.key === 'ArrowUp') {
+        //need to change existing positions of snake.body and add [x-1,y]
+        snake.body = snake.body.map(([row, col]) => [row - 1, col])
+    }
+    else if (event.key === 'ArrowRight') {
+        //need to change existing positions of snake.body and add [x,y+1]
+        snake.body = snake.body.map(([row, col]) => [row, col + 1])
+    }
+    else if (event.key === 'ArrowDown') {
+        //need to change existing positions of snake.body and add [x+1,y]
+        snake.body = snake.body.map(([row, col]) => [row + 1, col])
+    }
+    renderSnake()
+});
+
 
 // changes direction of the snake when an arrow key is clicked
-function changeDirection(event) {
-
-}
+// function changeDirection(event) {
+//     console.log(event)
+// }
 
 // render an apple at a random position on the board
 function randomPosApple() {
-
+    //random apple position 
+    let appleRow = Math.floor(Math.random() * gameState.boardSize.defaultRow)
+    let appleCol = Math.floor(Math.random() * gameState.boardSize.defaultColumn)
+    cells[appleRow][appleCol].classList.add('apple')
 }
+
 
 // Snake will grow in length when an apple is eaten
 function eatAppleAndGrow() {
@@ -70,26 +116,4 @@ function endGame() {
 
 }
 
-
-// REQUIREMENTS 
-// start the game by pressing a Start button
-// use my arrow keys to change the direction of the snake
-// have the snake grow correctly when it eats the apple
-// have the game end if the snake tries to eat itself
-// have the game end if the snake runs into a wall
-// see how long my snake was when the game ended
-// start the game over without having to reset the browser
-
-
-
-// // Constants for game board
-// const BOARD_WIDTH = 10;
-// const BOARD_HEIGHT = 10;
-
-// // Variables for game state
-// let snake = [];
-// let apple = {};
-// let direction = 'right';
-// let intervalId;
-// let score = 0;
 
